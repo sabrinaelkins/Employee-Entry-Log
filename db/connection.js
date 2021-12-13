ce0dfe6b44eb216fee9808e960ec5b1dc331add1
sabrinaelkins <sabrina.elkins12@gmail.com>
2021-12-13 16:11:06 -0600

    fixes to db

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createEmployeefinder({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'hql831$pah',
+    password: 'password',
     database: 'employeefinder'
   });
   
